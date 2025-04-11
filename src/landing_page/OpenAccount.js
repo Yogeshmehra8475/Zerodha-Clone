79e47c3f1495f850f5363c491bfe6f83dd614888
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-04-11 23:59:04 +0530

    Setting up the open account page, navbar, and footer

--- src/landing_page/OpenAccount.js
+++ src/landing_page/OpenAccount.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 function OpenAccount() {
     return ( 
-    <div className='container'>
-        <div className='row'></div>
-        <div className='col-6'></div>
-        <div className='col-6'></div>
-    </div>
+        <div className='container p-5 mb-5'>
+        <div className='row text-center'>
+            <h1 className='mt-5'>Open a Zerodha account</h1>
+            <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades..</p>
+            <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin: "0 auto"}}>Sign up for free</button>
+        </div>
+   </div>
      );
 }
 
