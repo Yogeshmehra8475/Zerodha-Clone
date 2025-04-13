b88acec6bb5b30121df074ca67183e1b36f81679
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-04-13 23:31:47 +0530

    Adding Not Found Page

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
 import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
+import NotFound from "./landing_page/NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,9 +20,10 @@ root.render(
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/about" element={<AboutPage />} />
-      <Route path="/product" element={<ProductsPage />} />
+      <Route path="/products" element={<ProductsPage />} />
       <Route path="/pricing" element={<PricingPage />} />
       <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
     <Footer/>
   </BrowserRouter>
