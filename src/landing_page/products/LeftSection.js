457137c69812cd32d974ce4cbbc60c2a9c4a1e2c
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-04-14 23:21:48 +0530

    Setup the Left Section of ProductPage using Props

--- src/landing_page/products/LeftSection.js
+++ src/landing_page/products/LeftSection.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
 
-function LeftSection() {
-    return ( 
-        <h1>LeftSection</h1>
-     );
+function LeftSection({
+  imageURl,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container">
+      <div className="row p-5">
+        <div className="col-4">
+          <img src={imageURl} />
+        </div>
+        <div className="col-2"></div>
+        <div className="col-6 p-5 ">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div>
+            <a href={tryDemo} style={{textDecoration:"none"}}>
+              Try Demo <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+            <a href={learnMore} style={{ marginLeft: "60px", textDecoration:"none"}}>
+              Learn More{" "}
+              <i class="fa fa-long-arrow-right" aria-hidden="true"></i>{" "}
+            </a>
+          </div>
+          <div className="mt-4">
+            <a href={googlePlay}>
+              <img src="media/images/googlePlayBadge.svg" />
+            </a>
+            <a href={appStore} style={{ marginLeft: "20px" }}>
+              <img src="media/images/appStoreBadge.svg" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftSection;
--- src/landing_page/products/ProductsPage.js
+++ src/landing_page/products/ProductsPage.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
+import Hero from "./Hero";
+import RightSection from "./RightSection";
+import LeftSection from "./LeftSection";
+import Universe from "./Universe";
 
 function ProductsPage() {
-    return (
-        <div>
-            <h1>ProductPage</h1>
-        </div>
-      );
+  return (
+    <>
+      <Hero />
+      <LeftSection
+        imageURl="media/images/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+      <LeftSection
+        imageURl="media/images/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+      <LeftSection
+        imageURl="media/images/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+      <Universe />
+    </>
+  );
 }
 
 export default ProductsPage;
