3ef195217e8bfdda5bf0322279289a7e673c3247
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-04-16 12:19:26 +0530

    Adding backend

---
+++ frontend/src/landing_page/support/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 ">
+        <h1 className="fs-4 ">To create a ticket, select a relevant topic</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
