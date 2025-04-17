5d6c563ec51ab0535dba5064a641fc22853ad688
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-04-17 22:05:03 +0530

    Adding required changes

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -4,10 +4,10 @@ function Hero() {
     return ( 
        <div className='container p-5 mb-5'>
             <div className='row text-center'>
-               <img src='media/images/homehero.png' alt='Hero Image' className='mb-5'/>
-                <h1 className='mt-5'>Invest in everything</h1>
-                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-                <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin: "0 auto"}}>Sign up for free</button>
+               <img src='media/images/homehero.png' alt='Hero Image' className='mb-3'/>
+                <h1 className='mt-3'>Invest in everything</h1>
+                <p className='fs-5 mt-2'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button className='p-2 btn btn-primary fs-5 mt-4' style={{width:"20%", margin: "0 auto"}}>Sign up for free</button>
             </div>
        </div>
      );
