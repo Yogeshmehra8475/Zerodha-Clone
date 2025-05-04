95936d732f73538ddd7bbf3c201278df93d61270
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-05-04 23:15:32 +0530

    Dashboard component

--- dashboard/src/components/TopBar.js
+++ dashboard/src/components/TopBar.js
@@ -15,6 +15,11 @@ const TopBar = () => {
               <p className='index-points'>{100.2}</p>
               <p className='percent'></p>
             </div>
+            <div className='sensex'>
+              <p className='index'>SENSEX</p>
+              <p className='index-points'>{100.2}</p>
+              <p className='percent'></p>
+            </div>
           </div>
 
           <Menu/>
