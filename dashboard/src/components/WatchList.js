053ef6a2cb2068ffb6ea4656eb6f3188500c033f
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-05-21 23:43:45 +0530

    Creating Watchlist Components

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -2,24 +2,59 @@ import React, { useState } from "react";
 
 import { Tooltip, Grow } from "@mui/material";
 
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
+
+import { watchlist } from "../data/data";
+
 const WatchList = () => {
-    return(
-        <div className="watchList-container">
-            <div className="search-container">
-                <input
-                    type="text"
-                    name="search"
-                    id="search"
-                    placeholder="Search ed:infy, bse, nifty fut weekly, gold mcx"
-                    className="search"
-                />
-                <span className="counts">9 / 50</span>
-            </div>
-            <ul className="List"></ul>
-        </div>
-
-    );
-}
-
+  return (
+    <div className="watchList-container">
+      <div className="search-container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search ed:infy, bse, nifty fut weekly, gold mcx"
+          className="search"
+        />
+        <span className="counts"> {watchlist.length} / 50</span>
+      </div>
+      <ul className="List">
+        {watchlist.map((stock, index) => {
+            return(
+          <WatchListItem stock={stock} key={index} />)
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatchlistActions, setshowWatchlistActions] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setshowWatchlistActions(true);
+  };
+  const handleMouseLeave = (e) => {
+    setshowWatchlistActions(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="down" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+    </li>
+  );
+};
