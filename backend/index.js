82235ebf2d9e7c653b8abcc382cd26de82d992ec
Yogesh Mehra <yogeshmehra2509@gmail.com>
2025-06-29 00:47:10 +0530

    Adding Functionalities

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,14 @@ const cors = require("cors");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
 const PORT = process.env.PORT || 3002;
 const url = process.env.MONGO_URL;
-
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
 
 // app.get("/addHoldings", async (req, res) => {
 //   let tempHoldings = [
@@ -190,6 +189,19 @@ app.get("/allHoldings", async (req, res) => {
 app.get("/allPositions", async (req, res) => {
   let allPositions = await PositionsModel.find({});
   res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty:req.body.qty,
+    price:req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
 });
 
 app.listen(PORT, () => {
